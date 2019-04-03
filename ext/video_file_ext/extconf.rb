56b15e5014d6b1c26ea880a47951bed1d5849cf7
furunkel <furunkel@users.noreply.github.com>
2019-04-03 13:07:54 +0200

    Fix copy-pase mistake

--- ext/video_file_ext/extconf.rb
+++ ext/video_file_ext/extconf.rb
@@ -2,7 +2,3 @@ require 'mkmf'
 pkg_config('libavutil libavformat libavcodec libswscale libturbojpeg')
 
 create_makefile('video_file_ext')
-
-require 'rake/extensiontask'
-
-Rake::ExtensionTask.new('video_file_ext')
